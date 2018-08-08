8d1a8bbca9b5a5856b36d3b0ed94c0175443fd69
liuxiaokun <liuxiaokun0410@qq.com>
2018-08-08 20:44:52 +0800

    IniRealm的方式

--- shiro-test/src/main/java/com/fred/shiro/test/TestIniRealm.java
+++ shiro-test/src/main/java/com/fred/shiro/test/TestIniRealm.java
@@ -4,35 +4,42 @@ import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authc.UsernamePasswordToken;
 import org.apache.shiro.mgt.DefaultSecurityManager;
 import org.apache.shiro.realm.SimpleAccountRealm;
+import org.apache.shiro.realm.text.IniRealm;
 import org.apache.shiro.subject.Subject;
 
-/**
- * @author liuxiaokun
- * 测试授权
- */
-public class TestAuthorization {
+public class TestIniRealm {
+
 
     public static void main(String[] args) {
 
-        SimpleAccountRealm simpleAccountRealm = new SimpleAccountRealm();
-        simpleAccountRealm.addAccount("fred", "123", "admin", "user");
+        IniRealm iniRealm = new IniRealm("classpath:user.ini");
 
         //1. 构建securityManager环境
         DefaultSecurityManager defaultSecurityManager = new DefaultSecurityManager();
-        defaultSecurityManager.setRealm(simpleAccountRealm);
+        defaultSecurityManager.setRealm(iniRealm);
 
         SecurityUtils.setSecurityManager(defaultSecurityManager);
         //2. 主体提交认证请求
         Subject subject = SecurityUtils.getSubject();
 
-        UsernamePasswordToken usernamePasswordToken = new UsernamePasswordToken("fred", "123");
+        UsernamePasswordToken usernamePasswordToken = new UsernamePasswordToken("wills", "123456");
         //登陆， 用户名不对：UnknownAccountException，密码不对：IncorrectCredentialsException
         subject.login(usernamePasswordToken);
 
         //true
         System.out.println(subject.isAuthenticated());
-        //校验不通过：UnauthorizedException: Subject does not have role [admin1]
+
         subject.checkRoles("admin", "user");
+
+        //权限验证
+        subject.checkPermissions("delete", "update");
+
+
+        //登出
+        subject.logout();
+        //false
+        System.out.println(subject.isAuthenticated());
+
 
     }
 }
